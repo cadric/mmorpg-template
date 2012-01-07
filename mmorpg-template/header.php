c22b5a97212dfb3be585bedf6e0f550b3216ac33
Thomas Severinsen <tseverinsen@gmail.com>
2012-01-07 15:45:31 +0100

    updated a few small things.

--- mmorpg-template/header.php
+++ mmorpg-template/header.php
@@ -1,10 +1,35 @@
-<!DOCTYPE html>
+<?php
+/**
+ * The Header for mmorpg-template.
+ * @package WordPress
+ * @subpackage mmorpg-template
+ * @since mmorpg-template 1.0
+ */
+?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<title>
-<?php wp_title (); ?>
-</title>
+<title><?php
+	/*
+	 * Print the <title> tag based on what is being viewed.
+	 */
+	global $page, $paged;
+
+	wp_title( '|', true, 'right' );
+
+	// Add the blog name.
+	bloginfo( 'name' );
+
+	// Add the blog description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) )
+		echo " | $site_description";
+
+	// Add a page number if necessary:
+	if ( $paged >= 2 || $page >= 2 )
+		echo ' | ' . sprintf( __( 'Page %s', 'twentyeleven' ), max( $paged, $page ) );
+
+	?></title>
 
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
